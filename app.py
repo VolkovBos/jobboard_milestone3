a8342c3d2d9fd1fce0d77fbef4e02d4a487615af
VolkovBos <volkovbos@gmail.com>
2020-10-01 21:58:49 +0000

    import flask error solved

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import os
 from flask import ( Flask, render_template )
 
-#in Heroku plaatsen
 app = Flask(__name__)
-app.secret_key = 'SECRET_KEY'
+app.secret_key = 'SECRET_KEY'           #in Heroku plaatsen
 
 
 @app.route('/')
@@ -11,8 +10,8 @@ app.secret_key = 'SECRET_KEY'
 def index():
     return render_template("index.html")
 
-#debug uitzetten aan het eind
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
-        debug=True) 
+        debug=True)                     #debug uitzetten aan het eind
