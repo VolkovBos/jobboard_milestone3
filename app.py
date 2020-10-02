70e4d3bb814300443287f634e6861efe5fc58b21
VolkovBos <volkovbos@gmail.com>
2020-10-02 09:50:25 +0000

    Mongo config added
    updated requirements.txt, app.py and added environment variables in env.py and created a .gitignore for env.py

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
+# General imports for this project
 import os
 from flask import ( Flask, render_template )
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
+# Import my env.py that's ignored by git
+if os.path.exists("env.py"):
+    import env
+
+
+# App instance
 app = Flask(__name__)
-app.secret_key = 'SECRET_KEY'           #in Heroku plaatsen
+SECRET_KEY = os.environ.get('SECRET_KEY')
+app.secret_key = SECRET_KEY
+
+
+# MongoDB configuration
+MONGO_URI = os.environ.get("MONGO_URI")
+app.config["MONGO_DBNAME"] = 'jobboard_milestone3'
+app.config["MONGO_URI"] = MONGO_URI
+mongo = PyMongo(app)
 
 
 @app.route('/')
@@ -11,6 +28,7 @@ def index():
     return render_template("index.html")
 
 
+# To run the app
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
