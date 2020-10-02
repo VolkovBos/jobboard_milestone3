47ca6f76da2f64233d9301261264a86ff43b5de5
VolkovBos <volkovbos@gmail.com>
2020-10-02 10:30:21 +0000

    Added contactMail.js and EmailJS to base.html

---
+++ static/js/contactMail.js
@@ -0,0 +1,24 @@
+function sendMail(contactForm) {
+    /* variables from EmailJS account */
+    var service_id = "gmail";
+    var template_id = "template_bos_up";
+
+    /* Sending template from EmailJS */
+    try {
+        emailjs.send(service_id, template_id, {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.email.value,
+            "message": contactForm.message.value,
+            "subject": contactForm.subject.value
+        });
+        $("#name, #email, #message, #subject").val("");
+        alert("Thank you for your message!");
+        return true;
+    
+    /* Error catching */
+    } catch (error) {
+        $("#name, #email, #message, #subject").val("");
+        alert(error);
+        return false;
+    }
+}
