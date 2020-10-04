476624fd3416ab34b12696612dd2ac4531b25c14
VolkovBos <volkovbos@gmail.com>
2020-10-04 20:39:21 +0000

    Added login route
    Used youtube tutorial https://www.youtube.com/watch?v=2Zz97NVbH0U

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 # General imports for this project
 import os
-from flask import ( Flask, render_template, url_for )
+from flask import (
+    flash,
+    Flask,
+    g,
+    redirect,
+    render_template,
+    request,
+    session, 
+    url_for 
+)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -22,6 +31,26 @@ app.config["MONGO_URI"] = MONGO_URI
 mongo = PyMongo(app)
 
 
+# Login config (https://www.youtube.com/watch?v=2Zz97NVbH0U)
+class User:
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
+
+# Set g.user at login to show the correct navbar and buttons 
+@app.before_request
+def before_request():
+    g.user = None
+
+    if 'user_id' in session:
+        g.user = mongo.db.candidates.find_one({'user_id': session['user_id']})
+
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -29,10 +58,33 @@ def index():
 
 
 # Login page for candidates and admin to login
-@app.route('/login')
+# I used a tutorial: https://www.youtube.com/watch?v=2Zz97NVbH0U for the login/session sections.
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        session.pop('user_id', None)
 
+        username = request.form['username']
+        password = request.form['password']
+
+        users = mongo.db.candidates.find_one({'user_name': username})
+
+        # Check if the username exists in the database
+        if bool(users):
+            # Check if the password is correct
+            if users["password"] ==  password:
+                session['user_id'] = users["user_id"] 
+                return redirect(url_for('index'))
+
+            else:
+                flash('Your password is incorrect')
+                return redirect(url_for('login'))
+
+        else:
+            flash('The username provided is not known')
+            return redirect(url_for('login'))
+
+    return render_template('login.html')
 
 # Registration page for new users
 @app.route('/register')
@@ -44,6 +96,7 @@ def register():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
 
 # Vacancies page for overview of open vacancies
 @app.route('/vacancies')
