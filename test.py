8965e3aed388bb0d22d96547ce8b271242ef4b12
VolkovBos <volkovbos@gmail.com>
2020-10-09 23:46:29 +0000

    Test added: profile page loads error visitor

--- test.py
+++ test.py
@@ -283,6 +283,15 @@ class FlaskTestCasesLoadsVisitor(unittest.TestCase):
         )
         self.assertTrue(b'<h1>404 Seems you got lost</h1>' in response.data)
 
+    # Ensure that the error page loads correctly
+    def test_profile(self):
+        tester = app.test_client(self)
+        response = tester.get(
+            '/profile',
+            content_type='html/text'
+        )
+        self.assertTrue(b'<h1>404 Seems you got lost</h1>' in response.data)
+
 
 # To run the test app
 if __name__ == "__main__":
