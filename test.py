ee0c6faf90456305faca169cf5dd2f9fa7d34118
VolkovBos <volkovbos@gmail.com>
2020-10-09 22:50:16 +0000

    Test added: edit vacancy route visitor

--- test.py
+++ test.py
@@ -127,6 +127,14 @@ class RoutesVisitor(unittest.TestCase):
         )
         self.assertEqual(response.status_code, 404)
 
+    # Ensure that a visitor gets the message that this page cannot be found
+    def test_editvacancy(self):
+        tester = app.test_client(self)
+        response = tester.get(
+            '/edit_vacancy',
+            content_type='html/text'
+        )
+        self.assertEqual(response.status_code, 404)
 
 # To run the test app
 if __name__ == "__main__":
