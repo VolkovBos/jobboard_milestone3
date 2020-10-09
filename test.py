d5b456b496e1213e7ed1842284e698d77d680361
VolkovBos <volkovbos@gmail.com>
2020-10-09 23:07:42 +0000

    Test added: addapplication page loads error  visitor

--- test.py
+++ test.py
@@ -211,6 +211,15 @@ class FlaskTestCasesLoadsVisitor(unittest.TestCase):
         )
         self.assertTrue(b'<h1>404 Seems you got lost</h1>' in response.data)
 
+    # Ensure that the error page loads correctly
+    def test_addapplicationFromApplicationPage(self):
+        tester = app.test_client(self)
+        response = tester.get(
+            '/add_application/admin',
+            content_type='html/text'
+        )
+        self.assertTrue(b'<h1>404 Seems you got lost</h1>' in response.data)
+
 
 # To run the test app
 if __name__ == "__main__":
