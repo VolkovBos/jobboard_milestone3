eb33030e2c07bf565352481d1a6c0ad47fd5bade
VolkovBos <volkovbos@gmail.com>
2020-10-09 12:34:35 +0000

    env.py added to test.py

--- test.py
+++ test.py
@@ -4,6 +4,11 @@ from app import app
 import unittest
 
 
+# Import my env.py that's ignored by git
+if os.path.exists("env.py"):
+    import env
+
+
 # To run the test app
 if __name__ == "__main__":
     unittest.main()
