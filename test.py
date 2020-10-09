cd62ef839ccd60fe52f1e0d70c6618564291c79b
VolkovBos <volkovbos@gmail.com>
2020-10-09 12:35:35 +0000

    First test added to test.py

--- test.py
+++ test.py
@@ -9,6 +9,14 @@ if os.path.exists("env.py"):
     import env
 
 
+class RoutesVisitor(unittest.TestCase):
+    # Ensure that route opens index page
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get('/index', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+
 # To run the test app
 if __name__ == "__main__":
     unittest.main()
