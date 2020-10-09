dc1815e52980aaff5249f7323e48dad384c5f056
VolkovBos <volkovbos@gmail.com>
2020-10-09 22:52:22 +0000

    Test added: users route visitor

--- test.py
+++ test.py
@@ -145,6 +145,14 @@ class RoutesVisitor(unittest.TestCase):
         )
         self.assertEqual(response.status_code, 404)
 
+    # Ensure that a visitor gets the message that this page cannot be found
+    def test_users(self):
+        tester = app.test_client(self)
+        response = tester.get(
+            '/users',
+            content_type='html/text'
+        )
+        self.assertEqual(response.status_code, 404)
 
 # To run the test app
 if __name__ == "__main__":
