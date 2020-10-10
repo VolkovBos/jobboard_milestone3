5656ce9ca2f16c4ec91bcdacc71c5e4b2a500d4c
VolkovBos <volkovbos@gmail.com>
2020-10-10 21:50:10 +0000

    Added test_profileRandomId visitor

--- test.py
+++ test.py
@@ -267,6 +267,15 @@ class RoutesVisitorUnavailable(unittest.TestCase):
         self.assertEqual(response.status_code, 404)
 
     # Ensure that a visitor gets the message that this page cannot be found
+    def test_profileRandomId(self):
+        tester = app.test_client()
+        response = tester.get(
+            '/profile/adbcdef',
+            content_type='html/text'
+        )
+        self.assertEqual(response.status_code, 404)
+
+    # Ensure that a visitor gets the message that this page cannot be found
     def test_users(self):
         tester = app.test_client()
         response = tester.get(
