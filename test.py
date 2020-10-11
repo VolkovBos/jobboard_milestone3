224b60f11c68d3275fd997ae858f228e58cb68e2
VolkovBos <volkovbos@gmail.com>
2020-10-11 13:37:53 +0000

    Added user loads test: test_addvacancy

--- test.py
+++ test.py
@@ -1126,6 +1126,21 @@ class LoadsUserUnavailable(unittest.TestCase):
         )
         self.assertTrue(b'<h1>404 Seems you got lost</h1>' in response.data)
 
+    # Ensure that the error page loads correctly
+    def test_addvacancy(self):
+        tester = app.test_client()
+        tester.post(
+            '/login',
+            data=dict(username=USERNAME_USER, password=SPW_TWO),
+            follow_redirects=True
+        )
+        response = tester.get(
+            '/add_vacancy',
+            content_type='html/text'
+        )
+        self.assertTrue(b'<h1>404 Seems you got lost</h1>' in response.data)
+
+
 # To run the test app
 if __name__ == "__main__":
     unittest.main()
