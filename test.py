9637696ef9f5ef8c8e072cfab805e50718a2c4a6
VolkovBos <volkovbos@gmail.com>
2020-10-11 00:35:43 +0000

    Added test_changepasswordRandomId user

--- test.py
+++ test.py
@@ -826,6 +826,20 @@ class RoutesUserUnavailable(unittest.TestCase):
         )
         self.assertEqual(response.status_code, 404)
 
+    # Ensure that a user gets the message that this page cannot be found
+    def test_changepasswordRandomId(self):
+        tester = app.test_client()
+        tester.post(
+            '/login',
+            data=dict(username=USERNAME_USER, password=SPW_TWO),
+            follow_redirects=True
+        )
+        response = tester.get(
+            '/change_password/abcdef',
+            content_type='html/text'
+        )
+        self.assertEqual(response.status_code, 500)
+
 
 # To run the test app
 if __name__ == "__main__":
