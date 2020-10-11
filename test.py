b44ee6c8167b46946b1f4c564f4bb9ceeb7bd45f
VolkovBos <volkovbos@gmail.com>
2020-10-11 13:39:29 +0000

    Added user loads test: test_editapplicationId

--- test.py
+++ test.py
@@ -1168,6 +1168,20 @@ class LoadsUserUnavailable(unittest.TestCase):
         )
         self.assertTrue(b'<h1>Error 500, Server error</h1>' in response.data)
 
+    # Ensure that the error page loads correctly
+    def test_editapplicationId(self):
+        tester = app.test_client()
+        tester.post(
+            '/login',
+            data=dict(username=USERNAME_USER, password=SPW_TWO),
+            follow_redirects=True
+        )
+        response = tester.get(
+            f'/edit_application/{APPLICATION_ID}',
+            content_type='html/text'
+        )
+        self.assertTrue(b'<h1>404 Seems you got lost</h1>' in response.data)
+
 
 # To run the test app
 if __name__ == "__main__":
