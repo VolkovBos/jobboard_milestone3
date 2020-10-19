0818750cf6e4e0b9f115a152266247b02313b71b
VolkovBos <volkovbos@gmail.com>
2020-10-19 17:16:25 +0000

    added preview image also on vacancy and application pages

--- static/js/loadImage.js
+++ static/js/loadImage.js
@@ -2,7 +2,19 @@ $('#image_url_button').on({
     'click': function(){
         var image_url = $("#image_url").val()
         try {
-            $('#blah').attr('src', image_url);
+            $('#render').attr('src', image_url);
+
+        } catch(err) {
+            return null
+        }
+    }
+});
+
+$('.render-image').on({
+    'change': function(){
+        try {
+            $("img").remove()
+            $('#add-image').prepend('<img class="card-img-top" id="render" src="' + this.value + '" alt="Vacancy image" />')
 
         } catch(err) {
             return null
